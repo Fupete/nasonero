02a3d170d03f1300d74477de7c890ed16733db8e
Daniele T <Fupete@users.noreply.github.com>
2023-04-18 11:02:48 +0200

    fix: pagefind js module

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,3 +5,10 @@ import 'focus-visible'
 
 // Internal Modules
 import './modules/nav'
+
+// Pagefind
+import './modules/pagefind-ui' 
+
+// const pagefind = await import("/nasonero/_pagefind/pagefind.js");
+
+{/* <script src="/_pagefind/pagefind-ui.js" onload="new PagefindUI({ element: '#search', showImages: false });"></script> */}
