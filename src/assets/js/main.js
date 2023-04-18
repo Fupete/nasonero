77476ca652ea45719230244c34762077b9a2bb9b
Daniele T <Fupete@users.noreply.github.com>
2023-04-18 11:09:48 +0200

    fix: pagefind dinamic url

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,10 +5,3 @@ import 'focus-visible'
 
 // Internal Modules
 import './modules/nav'
-
-// Pagefind
-import './modules/pagefind-ui' 
-
-// const pagefind = await import("/nasonero/_pagefind/pagefind.js");
-
-{/* <script src="/_pagefind/pagefind-ui.js" onload="new PagefindUI({ element: '#search', showImages: false });"></script> */}
